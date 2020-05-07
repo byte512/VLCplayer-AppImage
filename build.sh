2766b45043bbfea7451acaff5acc443e894ead90
cmatomic <33186553+cmatomic@users.noreply.github.com>
2020-05-07 18:04:02 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -57,7 +57,7 @@ apt-get --yes install curl build-essential autoconf  libxcb-image0-dev libxcb-im
   wget https://get.videolan.org/libdvdnav/6.1.0/libdvdnav-6.1.0.tar.bz2
   tar -xvjf  libdvdnav-6.1.0.tar.bz2
   cd libdvdnav-6.1.0
-  ./configure  --prefix=/usr --disable-static  --docdir=/usr/share/doc/libdvdnav-6.1.0 .
+  ./configure  --prefix=/usr --docdir=/usr/share/doc/libdvdnav-doc --enable-static --disable-silent-rules  .
   make -j$(nproc)
   make -j$(nproc) install
 )
@@ -67,7 +67,7 @@ apt-get --yes install curl build-essential autoconf  libxcb-image0-dev libxcb-im
   wget https://get.videolan.org/libdvdread/6.1.1/libdvdread-6.1.1.tar.bz2
   tar -xvjf  libdvdread-6.1.1.tar.bz2
   cd libdvdread-6.1.1
-  ./configure  --prefix=/usr --disable-static  --docdir=/usr/share/doc/libdvdread-6.1.1 .
+  ./configure  --prefix=/usr --docdir=/usr/share/doc/libdvdread-6.1.1 --enable-static  .
   make -j$(nproc)
   make -j$(nproc) install
 )
