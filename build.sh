25ff1abbdc1344990b5453415d1ecd6cf6a0294d
cmatomic <33186553+cmatomic@users.noreply.github.com>
2020-09-23 14:49:25 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -46,17 +46,6 @@ export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
  
 )
 
-(
-
- wget https://github.com/intel/libva/releases/download/2.7.1/libva-2.7.1.tar.bz2
- tar -xvjf libva-2.7.1.tar.bz2
- cd libva-2.7.1
-./autogen.sh
-./configure --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu
- make -j$(nproc)
- make -j$(nproc) install
-
-)
 
 (
   git clone https://github.com/videolabs/libdsm.git
